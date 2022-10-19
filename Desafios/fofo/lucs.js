c5cf0d8ceeed3f651f6e56334034d7795b32dcc4
Lucas Casarim de Lima <116107342+Casarimdev@users.noreply.github.com>
2022-10-19 11:31:29 -0300

    lucsnovo

---
+++ Desafios/fofo/lucs.js
@@ -0,0 +1,13 @@
+$("#change").click(function() {
+    $("#change").html("Sim");
+    $("#move").show();
+
+});
+
+function change()
+{
+    var i = Math.floor(Math.random()*500)+1;
+    var j = Math.floor(Math.random()*100)+1;
+    $("#move").css({top: j+'px', left:    i+'px'});
+
+}
